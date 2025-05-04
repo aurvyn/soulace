b9a33377bca6feff835d6c42cb5e6351231a5c6e
aurvyn <admin@dysrakescence.eu.org>
2025-05-04 01:31:34 -0400

    ADD: yaml + sass starting point

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,57 @@
-use eframe::egui;
+use std::fs;
+use eframe::egui::{self, RichText};
+
+const FILE_PATH: &str = "test/home.yaml";
 
 fn main() -> eframe::Result {
+	let yaml_code = fs::read_to_string(FILE_PATH).expect("Something went wrong reading the file");
+	let yaml = serde_yaml::from_str::<serde_yaml::Value>(&yaml_code).expect("Failed to parse YAML");
+	let doc = yaml
+		.as_mapping()
+		.expect("Failed to parse YAML as mapping");
+	let head = doc
+		.get("head")
+		.expect("Failed to get 'head' from YAML")
+		.as_mapping()
+		.expect("Failed to parse 'head' as mapping");
+	let title = head
+		.get("title")
+		.expect("Failed to get 'title' from 'head'")
+		.as_str()
+		.expect("Failed to parse 'title' as text")
+		.to_string();
+	let body = doc
+		.get("body")
+		.expect("Failed to get 'body' from YAML")
+		.as_sequence()
+		.expect("Failed to parse 'body' as sequence")
+		.clone();
+
 	let mut souls = 0;
 	let mut options = eframe::NativeOptions::default();
 	options.renderer = eframe::Renderer::Wgpu;
-	eframe::run_simple_native("Soulace", options, move |ctx, _frame| {
+	eframe::run_simple_native(&title, options, move |ctx, _frame| {
 		egui::CentralPanel::default().show(ctx, |ui| {
-			ui.heading("Welcome to the Soulace browser!");
-			ui.label("Soulace is a browser that uses Pug/Sass/Nim instead of HTML/CSS/JS.");
-			ui.horizontal(|ui| {
-				ui.label("It is a work in progress, and not all features are implemented yet.");
-				ui.label("This is just a test page.");
-			});
+			for item in &body {
+				item.as_mapping().map(|mapping| {
+					let (tag, value) = mapping.iter().next().unwrap();
+					let tag = tag.as_str().expect("Failed to parse tag as valid syntax");
+					let value = value.as_str().expect("Failed to parse value as text");
+					let text = RichText::new(value);
+					ui.label(match tag {
+						"p" => text,
+						"h1" => text.size(30.0).strong(),
+						"h2" => text.size(28.0).strong(),
+						"h3" => text.size(26.0).strong(),
+						"h4" => text.size(24.0).strong(),
+						"h5" => text.size(22.0).strong(),
+						"h6" => text.size(20.0).strong(),
+						_ => panic!("Unknown tag: {}", tag),
+					})
+				}).unwrap_or_else(|| {
+					ui.label("Invalid item in YAML")
+				});
+			}
 			ui.label(format!("You have collected {} souls!", souls));
 			if ui.button("collect soul").clicked() {
 				souls += 1;
